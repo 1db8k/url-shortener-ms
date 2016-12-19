bf7f2d9aa9c74401a7a96a84fc731cb43c9c6f93
Your Name <you@example.com>
2016-12-19 11:40:47 +0100

    setup basic mongo connection

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const app = express()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const MONGOLAB_URI = process.env.MONGOLAB_URI
+
+app.use('/', express.static(__dirname + '/public'))
+
+MongoClient.connect(MONGOLAB_URI, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err)
+  } else {
+    console.log('Connection established database')
+
+    db.close()
+  }
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!')
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Example app listening on port 5000!')
+})
