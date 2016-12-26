dcd85cf3f4a48b3de042ec398916da69586ad4ab
Your Name <you@example.com>
2016-12-26 18:09:05 +0100

    tidied dependencies

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express')
 const app = express()
-// const url = require('url')
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const MONGOLAB_URI = process.env.MONGOLAB_URI
-// const encode = require('./encode')
-const routes = require('./routes')
-// const util = require('util')
-// const herokuDomain = 'https://url-tin.herokuapp.com/'
-// const apiPath = '/new/'
+const routes = require('./routes.promises')
 
 app.use('/', express.static(__dirname + '/public'))
 
-//  connect to db
 MongoClient.connect(MONGOLAB_URI)
-//  then listen to connections
-.then((db) => routes(app, db))
-//  then
-//  catch
-.catch((err) => console.error(err))
-
-// MongoClient.connect(MONGOLAB_URI, function (err, db) {
-//     // db.close()
-//   }
-// })
+  .then((db) => routes(app, db))
+  .catch((err) => console.error(err))
